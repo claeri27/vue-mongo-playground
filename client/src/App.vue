eff440f08c16db25b38506fc1f2e5ab59941f22c
Christopher Laeri <claeri27@gmail.com>
2019-01-17 00:08:59 -0500

    minor changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,7 @@
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
         <span class="font-weight-light">Crypto</span>
-        <span>FLEX</span>
+        <span>VEXX</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-menu offset-y left>
